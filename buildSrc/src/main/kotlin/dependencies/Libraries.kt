f592a4790c6b834c8c96e3069e80faf30743e939
Jose Maria Payá Castillo <josepaya17@gmail.com>
2020-11-24 16:04:05 +0100

    Update kotlin dependencies to 1.4.20

--- buildSrc/src/main/kotlin/dependencies/Libraries.kt
+++ buildSrc/src/main/kotlin/dependencies/Libraries.kt
@@ -42,7 +42,7 @@ object Libraries {
         const val FRAGMENT = "1.2.5"
         const val HILT = "2.28-alpha"
         const val HILT_VIEWMODEL = "1.0.0-alpha01"
-        const val KOTLIN = "1.4.10"
+        const val KOTLIN = "1.4.20"
         const val LICENSES_DIALOG = "2.1.0"
         const val LIFECYCLE = "2.2.0"
         const val MATERIAL = "1.2.1"
