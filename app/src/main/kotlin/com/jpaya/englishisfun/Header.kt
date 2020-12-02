88bd48317dd5ae5c6623ef2ff8228d4514f84825
Jose Maria Payá Castillo <josepaya17@gmail.com>
2020-12-02 15:37:15 +0100

    Remove unused import

--- app/src/main/kotlin/com/jpaya/englishisfun/Header.kt
+++ app/src/main/kotlin/com/jpaya/englishisfun/Header.kt
@@ -20,7 +20,6 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.FrameLayout
-import android.widget.TextView
 import com.jpaya.englishisfun.databinding.HeaderBinding
 
 class Header @JvmOverloads constructor(
